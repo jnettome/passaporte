934a3ec1bf6c705b0b3e4a0d4e41aadb70ad0466
João Netto <hi@joaonetto.me>
2013-11-26 20:28:35 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Passaporte::Application.config.secret_key_base = '8a764c60504402ed8dd387a7a0f961f706ce2407aeb031855ca231c220e00f3b856fa52de10a586efd27de4f165a8e454d715aab0c4cb19ca5a5f91065885a0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Passaporte::Application.config.session_store :cookie_store, key: '_passaporte_session'
